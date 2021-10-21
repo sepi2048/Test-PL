5cfe40213f6384a5535c97af14634fbc840e1bba
sepi2048 <sepi2048@mailinator.com>
2021-10-21 19:19:52 +0200

    try/catch mailinglist

--- functions/email.js
+++ functions/email.js
@@ -140,15 +140,19 @@ exports.handler = async function(req, res) {
 
     // call mailingList API
     const mail = "ludde@gmail.com";
-    axios.put("https://stoic-payne-386d66.netlify.app/api/mailingList?mail="+mail)
-      .then((res) => {
-        callback(null, {
-          statusCode: 200,
-          body: res.data.title,
-        });
-      })
-      .catch((err) => {
-        callback(err);
-      });
+    let list = {};
+    try {
+        list = axios.put("https://stoic-payne-386d66.netlify.app/api/mailingList?mail="+mail);
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({
+                status: 'Added to mailinglist',
+            }),
+        }
+      } catch(err) {
+        console.error(err);
+        console.error(err.list.body);
+      }
 
 }
