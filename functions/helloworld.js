a7d4fdaf1370c147706eb20f2be19c5032f52bcb
sepi2048 <sepi2048@mailinator.com>
2021-10-21 18:18:24 +0200

    https://travishorn.com/netlify-lambda-functions-from-scratch-1186f61c659e

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,22 +1,14 @@
 const axios = require('axios');
 
-exports.handler = async function(event, context) {
-
-    // how to call nextjs API from with netlify function?
-
-    const mail = "helloworld@gmail.com";
-    // add to mailinglist
-    return axios
-    .put("/api/mailingList?mail="+mail)
-    .then((result) => {
-      if (result.status === 200) {
-        toast.success(result.data.message);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-    
-
-}
+exports.handler = (event, context, callback) => {
+    axios.put("/api/mailingList?mail="+mail)
+      .then((res) => {
+        callback(null, {
+          statusCode: 200,
+          body: res.data.title,
+        });
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  };
