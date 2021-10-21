0b6237fd062a3799e0f6a868761811a0e0d9e5c5
sepi2048 <sepi2048@mailinator.com>
2021-10-21 19:27:39 +0200

    try/catch helloworld

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -4,7 +4,8 @@ const mail = "helloworld@gmail.com";
 
 
 exports.handler = (event, context, callback) => {
-    axios.put("https://stoic-payne-386d66.netlify.app/api/mailingList?mail="+mail)
+
+    /* axios.put("https://stoic-payne-386d66.netlify.app/api/mailingList?mail="+mail)
       .then((res) => {
         callback(null, {
           statusCode: 200,
@@ -13,5 +14,25 @@ exports.handler = (event, context, callback) => {
       })
       .catch((err) => {
         callback(err);
-      });
+      });*/
+
+
+    // call mailingList API
+    const mail = "ludde@gmail.com";
+    let list = {};
+    try {
+        list = axios.put("https://stoic-payne-386d66.netlify.app/api/mailingList?mail="+mail);
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({
+                status: 'Added to mailinglist',
+            }),
+        }
+      } catch(err) {
+        console.error(err);
+        console.error(err.list.body);
+      }
+
+
   };
