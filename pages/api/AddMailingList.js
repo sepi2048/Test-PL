f3db06037d12113e5e33434fa9a8298302179ec4
sepi2048 <sepi2048@mailinator.com>
2021-11-08 09:05:36 +0100

    res.status(202).send({

--- pages/api/AddMailingList.js
+++ pages/api/AddMailingList.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
        }
      )
      .then((result) => {
-      result.status(202).send({
+      res.status(202).send({
          message:
            "Your email has been succesfully added to the mailing list. Welcome 👋",
        });
