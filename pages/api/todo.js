a59ea7cd03de9422a755745186d59860444715e0
sepi2048 <sepi2048@mailinator.com>
2021-11-09 10:20:47 +0100

    hello example

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -1,18 +1,8 @@
-const axios = require('axios');
-
-// https://travishorn.com/netlify-lambda-functions-from-scratch-1186f61c659e
-
-exports.handler = function(event, context, callback) {
-  axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: res.data.title,
-      });
-    })
-    .catch((err) => {
-      callback(err);
-    });
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: 'Hello, world!',
+  });
 };
 
 
