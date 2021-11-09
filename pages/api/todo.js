67f2a54cf3a510db530ffe762be0618a3a73b3f7
sepi2048 <sepi2048@mailinator.com>
2021-11-09 10:55:03 +0100

    nextjs syntax

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -1,9 +1,27 @@
+const axios = require('axios');
+
 export default async function handler(req, res) {
-  return {
-    statusCode: 200,
-    body: 'Hello, world!',
-    message: 'Yo',
-  };
+  try {
+    const result = axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    res.status(200).send(result.data.title)
+  } catch (err) {
+    res.status(500).json({ error: 'failed to load data' })
+  }
 }
 
 
+
+
+
+exports.handler = (event, context, callback) => {
+  axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: res.data.title,
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
