1365fe700613d30e8731302a7db354138f1f2c33
sepi2048 <sepi2048@mailinator.com>
2021-11-09 10:58:04 +0100

    nextjs syntax console

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 export default async function handler(req, res) {
   try {
     const result = axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    res.status(200).send(result.data.title)
+    res.status(200).send(result)
+    console.log(result)
   } catch (err) {
     res.status(500).json({ error: 'failed to load data' })
   }
