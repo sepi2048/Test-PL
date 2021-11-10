c9e6e403aa6c6606ef697574057f8f8040ae0cd2
sepi2048 <sepi2048@mailinator.com>
2021-11-10 11:38:26 +0100

    res.json(

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const result = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(result))
+  res.json(result)
 }
 
 
