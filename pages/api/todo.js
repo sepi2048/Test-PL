3cccfae06b167cd9a565c497866499a4ff3153c3
sepi2048 <sepi2048@mailinator.com>
2021-11-10 10:46:08 +0100

    await axios

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -2,10 +2,13 @@ const axios = require('axios');
 
 export default async function handler(req, res) {
   try {
-    const result = axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    const result = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
     res.status(200).send(result)
     console.log(result)
   } catch (err) {
     res.status(500).json({ error: 'failed to load data' })
   }
 }
+
+
+
