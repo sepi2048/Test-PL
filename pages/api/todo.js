07104546deab15a6247ec62d73c223c5563988c7
sepi2048 <sepi2048@mailinator.com>
2021-11-10 11:48:59 +0100

    await fix

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 
+// https://stackoverflow.com/questions/65859582/how-to-convert-lambda-function-into-a-next-js-api-friendly-function
+
 export default async function handler(req, res) {
-  const result = awaitaxios.get('https://jsonplaceholder.typicode.com/todos/1')
+  const result = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
   res.statusCode = 200
   res.send(result)
 }
