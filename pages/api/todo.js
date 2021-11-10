bc3d364e6922682b87e5f7d7c5fab5b65d7362f7
sepi2048 <sepi2048@mailinator.com>
2021-11-10 12:48:40 +0100

    try-catch

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -3,9 +3,15 @@ const axios = require('axios');
 // https://stackoverflow.com/questions/65859582/how-to-convert-lambda-function-into-a-next-js-api-friendly-function
 
 export default async function handler(req, res) {
-  const result = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-  res.statusCode = 200
-  res.send(result.data.title)
+
+  try {
+    const result = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    res.statusCode = 200
+    res.send(result.data.title)
+  } catch (err) {
+      console.error('Error from function: ', err)
+  }
+
 }
 
 
