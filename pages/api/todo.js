6c85acc31b25676db776177b9bb8cf36d8fffda6
sepi2048 <sepi2048@mailinator.com>
2021-11-10 10:40:53 +0100

    req, res nextjs

--- pages/api/todo.js
+++ pages/api/todo.js
@@ -9,20 +9,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'failed to load data' })
   }
 }
-
-
-
-
-
-exports.handler = (event, context, callback) => {
-  axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: res.data.title,
-      });
-    })
-    .catch((err) => {
-      callback(err);
-    });
-};
