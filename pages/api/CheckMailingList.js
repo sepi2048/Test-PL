1dc2c7411fa1192a92ca1f747fabdc133b75156e
sepi2048 <sepi2048@mailinator.com>
2021-11-12 15:57:20 +0100

    try-catch

--- pages/api/CheckMailingList.js
+++ pages/api/CheckMailingList.js
@@ -7,6 +7,9 @@
 * error decoding lambda response: invalid status code returned from lambda: 0?
 * Checkout checkbox? (tutorial)
 * Correct error handelig? (getPDF and PDFWatermark)
+* Test Scenario:
+* 1) Move coffee from NEWSLETTER to NEWSLETTER PAYED (if else)
+* 2) Add Isabelle to NEWSLETTER PAYED (else)
 */
 
 const axios = require('axios');
@@ -18,88 +21,105 @@ export default async function handler(req, res) {
   console.log("CheckMailinglist: " + mail + " " + list_id);
 
 
- if (req.method === "POST") {
+  if (req.method === "POST") {
 
-  if (list_id  === process.env.SENDGRID_MAILING_ID_NEWSLETTER) {
+    if (list_id  === process.env.SENDGRID_MAILING_ID_NEWSLETTER) {
 
-    try {
+      try {
 
-      // SEARCH: does email already exists in NEWSLETTER PAYED?
-      const search_result = await axios.post("https://stoic-payne-386d66.netlify.app/api/SearchMailingList?mail="+mail+"&list_id="+process.env.SENDGRID_MAILING_ID_NEWSLETTER_PURCHASE)
-    
-      console.log(search_result.data.contact_count);
+        // SEARCH: does email already exists in NEWSLETTER PAYED?
+        const search_result = await axios.post("https://stoic-payne-386d66.netlify.app/api/SearchMailingList?mail="+mail+"&list_id="+process.env.SENDGRID_MAILING_ID_NEWSLETTER_PURCHASE)
+      
+        console.log(search_result.data.contact_count);
 
-      // SEARCH RESULT: email does already exists in NEWSLETTER PAYED
-      if (search_result.data.contact_count >= 1 ) {
-          res.send({
-            message:
-            "You are already subscribed to this newsletter",
-          });
-      } else {
+        // SEARCH RESULT: email does already exists in NEWSLETTER PAYED
+        if (search_result.data.contact_count >= 1 ) {
+            res.send({
+              message:
+              "You are already subscribed to NEWSLETTER PAYED",
+            });
+        } else {
+          try {
+            // Email does NOT already exists in NEWSLETTER PAYED, add to NEWSLETTER
+            await axios.put("https://stoic-payne-386d66.netlify.app/api/AddMailingList?mail="+mail+"&list_id="+list_id)
+            res.send({
+              message:
+              "You are added to NEWSLETTER",
+            });
+          } catch (err) {
+              console.error('Could not add email to NEWSLETTER: ', err)
+          }
+        }
+
+      } catch (err) {    
+          console.error('There was a problem with checking your NEWSLETTER PAYED subscription: ', err)
+      }
+
+
+    } else if (list_id  === process.env.SENDGRID_MAILING_ID_NEWSLETTER_PURCHASE)  {
+
+
         try {
-          // Email does NOT already exists in NEWSLETTER PAYED, add to NEWSLETTER
+
+        // SEARCH: does email already exists in NEWSLETTER?
+        await axios.post("https://stoic-payne-386d66.netlify.app/api/SearchMailingList?mail="+mail+"&list_id="+process.env.SENDGRID_MAILING_ID_NEWSLETTER)
+
+        //console.log(result.data.result[0].id);
+
+        // SEARCH RESULT: email does already exists in NEWSLETTER
+        if (result.data.contact_count >= 1 ) {
+
+          try {  
+            // REMOVE from NEWSLETTER
+            axios.delete(
+              "https://api.sendgrid.com/v3/marketing/lists/"+process.env.SENDGRID_MAILING_ID_NEWSLETTER+"/contacts?contact_ids="+result.data.result[0].id,
+            {
+              headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+              },
+            }
+            ) 
+
+            console.log("Removed from NEWSLETTER");
+
+          } catch (err) {
+            console.error('Could not be remove from NEWSLETTER: ', err)
+          }  
+
+        }
+        // Email does NOT already exists in NEWSLETTER (also deleted), add to NEWSLETTER PAYED
+        try {
           await axios.put("https://stoic-payne-386d66.netlify.app/api/AddMailingList?mail="+mail+"&list_id="+list_id)
           res.send({
             message:
-            "You are added to this newsletter",
+            "You are already added to NEWSLETTER PAYED (also hidden transfer)",
           });
         } catch (err) {
-            console.error('Could not add email to malinglist: ', err)
+            console.error('Could not add email to NEWSLETTER PAYED: ', err)
         }
+
+
+      } catch (err) {    
+        console.error('There was a problem with checking NEWSLETTER subscription: ', err)
       }
 
-    } catch (err) {    
-        console.error('There was a problem with checking your subscription: ', err)
-    }
+     
+     } else {
 
-
-  } else if (list_id  === process.env.SENDGRID_MAILING_ID_NEWSLETTER_PURCHASE)  {
-
-    // SEARCH: does email already exists in NEWSLETTER?
-    axios.post("https://stoic-payne-386d66.netlify.app/api/SearchMailingList?mail="+mail+"&list_id="+process.env.SENDGRID_MAILING_ID_NEWSLETTER)
-
-    .then((result) => {
-
-      //console.log(result.data.result[0].id);
-
-      // SEARCH RESULT: email does already exists in NEWSLETTER
-      if (result.data.contact_count >= 1 ) {
-        console.log("Inside else-if if statement");
-        // REMOVE from NEWSLETTER
-        axios.delete(
-          "https://api.sendgrid.com/v3/marketing/lists/"+process.env.SENDGRID_MAILING_ID_NEWSLETTER+"/contacts?contact_ids="+result.data.result[0].id,
-        {
-          headers: {
-            "content-type": "application/json",
-            Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-          },
-        }
-        ) .then((result) => { 
-          res.status(202).write({
-            message:
-              "Email removed from Newsletter Signup Form",
-          }); 
-        });
-      } 
-
-      // Email does NOT already exists in NEWSLETTER (also deleted), add to NEWSLETTER PAYED
-      axios.put("https://stoic-payne-386d66.netlify.app/api/AddMailingList?mail="+mail+"&list_id="+list_id)
-
-      })
-      .catch((err) => {
-        res.status(500).send({
+      // list_id Email NOT NEWSLETTER or NEWSLETTER PAYED
+      try {
+        await axios.put("https://stoic-payne-386d66.netlify.app/api/AddMailingList?mail="+mail+"&list_id="+list_id)
+        res.send({
           message:
-            "Oups, there was a problem with checking your subscription",
+          "You are added to this newsletter",
         });
-        console.error(err);
-      });
+      } catch (err) {
+          console.error('Could not add email to malinglist: ', err)
+      }
 
-  } else {
 
-    // list_id Email NOT NEWSLETTER or NEWSLETTER PAYED
-    axios.put("https://stoic-payne-386d66.netlify.app/api/AddMailingList?mail="+mail+"&list_id="+list_id)
-
-  }
+    }
 
   }
 }
